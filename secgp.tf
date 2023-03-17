dc050f43aae930f381fb9c6df28e3014bc99c05f
manu240586 <wesofttech3@gmail.com>
2023-03-17 06:11:41 +0530

    nlnn

---
+++ secgp.tf
@@ -0,0 +1,85 @@
+resource "aws_security_group" "vprofile_bean_sg" {
+  Name = "vprofile_bean_sg"
+  description = "Security group for Beanstalk"
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "vprofile_bastion_sg" {
+  name = "vprofile_bastion_sg"
+  description = "Security group for bastionisioner"
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = [var.myip]
+
+  }
+}
+resource "aws_security_group" "vprofile_Beans_ec2_sg" {
+  name = "vprofile_Beans_ec2_sg"
+  description = "vprofile Beans ec2sg"
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    security_groups = [aws_security_group.vprofile_bastion_sg.id]
+
+  }
+
+}
+
+resource "aws_security_group" "vprofile_backend_sg" {
+  name = "vprofile_backend_sg"
+  description = "vprofile backend sg"
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 0
+    protocol  = "tcp"
+    to_port   = 0
+    security_groups = [aws_security_group.vprofile_Beans_ec2_sg.id]
+
+  }
+}
+resource "aws_security_group_rule" "backend_sg_allow_itself" {
+  from_port         = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vprofile_backend_sg.id
+  source_security_group_id = aws_security_group.vprofile_backend_sg.id
+  to_port           = 65535
+  type              = "ingress"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,19 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  azs = [var.zone1,var.zone2]
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-  private_subnets = [var.priv_sub_cidr_1,var.priv_sub_cidr_2]
-  public_subnets = [var.pub_sub_cidr_1,var.pub_sub_cidr_2]
+  source          = "terraform-aws-modules/vpc/aws"
+  azs             = [var.zone1, var.zone2]
+  name            = var.vpc_name
+  cidr            = var.vpc_cidr
+  private_subnets = [var.priv_sub_cidr_1, var.priv_sub_cidr_2]
+  public_subnets  = [var.pub_sub_cidr_1, var.pub_sub_cidr_2]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
 
-    terraform = "true"
+    terraform   = "true"
     environment = "true"
 
 
