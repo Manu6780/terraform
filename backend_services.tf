e6a5f237504c962026b993c37b5daf200d28c571
manu240586 <wesofttech3@gmail.com>
2023-03-19 14:54:41 +0530

    wvnllbnl

--- backend_services.tf
+++ backend_services.tf
@@ -36,7 +36,7 @@ resource "aws_elasticache_cluster" "vprofile_cache" {
   node_type            = "cache.t2.micro"
   engine_version       = "1.5.16"
   parameter_group_name = "default.memcached1.5"
-  num_cache_nodes = 1
+  num_cache_nodes      = 1
   port                 = 11211
   security_group_ids   = [aws_security_group.vprofile_backend_sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.Vprofile_ecache_subgrp.name
